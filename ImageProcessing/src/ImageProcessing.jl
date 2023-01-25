909375a9272752f52f496a74d3938a7d7424cfa7
Vadim <vadim.bertrand@lilo.org>
2023-01-25 17:52:27 +0100

    add docstring

--- ImageProcessing/src/ImageProcessing.jl
+++ ImageProcessing/src/ImageProcessing.jl
@@ -2,18 +2,35 @@ module ImageProcessing
 
 # Julia call
 include("conv.jl")
+"""
+    jl_apply_filter(img::Matrix, filter::Matrix{Int8})
+
+Apply filter `filter` to the image `img` using Julia implementation.
+
+# Arguments
+- `img::Matrix`: The image to transform
+- `filter::Matrix{Int8}`: The filter to apply
+"""
 function jl_apply_filter(img::Matrix, filter::Matrix{Int8})
     return @timed conv(img, filter)
 end
 
 # Python call
-
 using PyCall
 py"""
 import sys
 sys.path.insert(0, "../ImageProcessing/src")
 """
 const py_conv = pyimport("conv")["conv"]
+"""
+    py_apply_filter(img::Array{Float64}, filter::Matrix{Int8})
+
+Apply filter `filter` to the image `img` using Python implementation.
+
+# Arguments
+- `img::Array{Float64}`: The image to transform
+- `filter::Matrix{Int8}`: The filter to apply
+"""
 function py_apply_filter(img::Array{Float64}, filter::Matrix{Int8})
     return py_conv(img, filter)
 end
@@ -27,11 +44,28 @@ if (!("imager" %in% rownames(installed.packages()))){
 }
 source("../ImageProcessing/src/conv.R")
 """
+"""
+    py_apply_filter(img::Array{Float64}, filter::Matrix{Int8})
+
+Apply filter `filter` to the image `img` using R implementation.
+
+# Arguments
+- `img::Array{Float64}`: The image to transform
+- `filter::Matrix{Int8}`: The filter to apply
+"""
 function r_apply_filter(img::Array{Float64}, filter::Matrix{Int8})
     img, time = rcall(:conv, img, filter)
     return img, rcopy(time)
 end
 
+"""
+    img_to_arr(img::Matrix)
+
+Convert the image `img` from a RGB matrix to a Float64 matrix.
+
+# Arguments
+- `img::Matrix`: The image to convert
+"""
 function img_to_arr(img::Matrix)
     arr = channelview(img)
     if length(img) == 3
@@ -41,6 +75,14 @@ function img_to_arr(img::Matrix)
 end
 
 # Filters
+"""
+    get_filter(filter_name::String)
+
+Returns the matrix representing the filter corresponding to the name `filter_name`.
+
+# Arguments
+- `filter_name::String`: The name of filter to return
+"""
 function get_filter(filter_name::String)
     filter = ones(Int8, (3, 3))
     if filter_name == "flou"
@@ -61,6 +103,16 @@ function get_filter(filter_name::String)
 end
 
 # Main entry
+"""
+    apply_filter(img::Matrix, filter_name::String, lang::String)
+
+Apply filter `filter` to the image `img` using the language `lang` implementation.
+
+# Arguments
+- `img::Matrix`: The image to transforme
+- `filter_name::String`: The name of the filter to apply
+- `lang::String`: The language implementation to use
+"""
 function apply_filter(img::Matrix, filter_name::String, lang::String)
     filter = get_filter(filter_name)
     if lang == "julia"
